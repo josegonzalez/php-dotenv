e6b9b6aeed160a7cfea875272bbcefc867e7a51e
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-12-29 22:36:25 -0500

    PHPCS fixes

--- src/josegonzalez/Dotenv/Load.php
+++ src/josegonzalez/Dotenv/Load.php
@@ -7,181 +7,181 @@ use LogicException;
 use JsonSerializable;
 use RuntimeException;
 
-class Load implements JsonSerializable {
+class Load implements JsonSerializable
+{
 
-  protected $_filepath = null;
+    protected $_filepath = null;
 
-  protected $_environment = null;
+    protected $_environment = null;
 
-  public function __construct($filepath = null) {
-    $this->setFilepath($filepath);
-    return $this;
-  }
-
-  public function filepath() {
-  	return $this->_filepath;
-  }
-
-  public function setFilepath($filepath = null) {
-  	if ($filepath == null) {
-      $filepath = __DIR__ . DIRECTORY_SEPARATOR . '.env';
-    }
-    $this->_filepath = $filepath;
-    return $this;
-  }
-
-  public static function load($options = null) {
-    $filepath = null;
-    if (is_string($options)) {
-      $filepath = $options;
-      $options = array();
+    public function __construct($filepath = null) {
+        $this->setFilepath($filepath);
+        return $this;
     }
 
-    $dotenv = new \josegonzalez\Dotenv\Load($filepath);
-    $dotenv->parse();
-    if (array_key_exists('expect', $options)) {
-      $dotenv->expect($options['expect']);
+    public function filepath() {
+        return $this->_filepath;
     }
 
-    if (array_key_exists('define', $options)) {
-      $dotenv->define();
+    public function setFilepath($filepath = null) {
+        if ($filepath == null) {
+            $filepath = __DIR__ . DIRECTORY_SEPARATOR . '.env';
+        }
+        $this->_filepath = $filepath;
+        return $this;
     }
 
-    if (array_key_exists('toEnv', $options)) {
-      $dotenv->toEnv($options['toEnv']);
+    public static function load($options = null) {
+        $filepath = null;
+        if (is_string($options)) {
+            $filepath = $options;
+            $options = array();
+        }
+
+        $dotenv = new \josegonzalez\Dotenv\Load($filepath);
+        $dotenv->parse();
+        if (array_key_exists('expect', $options)) {
+            $dotenv->expect($options['expect']);
+        }
+
+        if (array_key_exists('define', $options)) {
+            $dotenv->define();
+        }
+
+        if (array_key_exists('toEnv', $options)) {
+            $dotenv->toEnv($options['toEnv']);
+        }
+
+        if (array_key_exists('toServer', $options)) {
+            $dotenv->toServer($options['toServer']);
+        }
+
+        return $dotenv;
     }
 
-    if (array_key_exists('toServer', $options)) {
-      $dotenv->toServer($options['toServer']);
+    public function parse() {
+        if (!file_exists($this->_filepath)) {
+            throw new InvalidArgumentException(sprintf("Environment file '%s' is not found.", $this->_filepath));
+        }
+
+        if (!is_readable($this->_filepath)) {
+            throw new InvalidArgumentException(sprintf("Environment file '%s' is not readable.", $this->_filepath));
+        }
+
+        $fc = file_get_contents($this->_filepath);
+        if ($fc === false) {
+            throw new InvalidArgumentException(sprintf("Environment file '%s' is not readable.", $this->_filepath));
+        }
+
+        $lines = explode(PHP_EOL, $fc);
+
+        $this->_environment = array();
+        foreach ($lines as $line) {
+            if (empty($line)) {
+                continue;
+            }
+
+            if (!preg_match('/(?:export )?([a-zA-Z_][a-zA-Z0-9_]*)=(.*)/', $line, $matches)) {
+                continue;
+            }
+
+            $key = $matches[1];
+            $value = $matches[2];
+            if (preg_match('/^\'(.*)\'$/', $value, $matches)) {
+                $value = $matches[1];
+            } elseif (preg_match('/^"(.*)"$/', $value, $matches)) {
+                $value = $matches[1];
+            }
+
+            $this->_environment[$key] = $value;
+        }
+
+        return $this;
     }
 
-    return $dotenv;
-  }
+    public function expect() {
+        $this->requireParse('expect');
 
-  public function parse() {
-    if (!file_exists($this->_filepath)) {
-      throw new InvalidArgumentException(sprintf("Environment file '%s' is not found.", $this->_filepath));
+        $args = func_get_args();
+        if (count($args) == 0) {
+            throw new InvalidArgumentException("No arguments were passed to expect()");
+        }
+
+        if (isset($args[0]) && is_array($args[0])) {
+            $args = $args[0];
+        }
+
+        $keys = (array) $args;
+        $missingEnvs = array();
+
+        foreach ($keys as $key) {
+            if (!isset($this->_environment[$key])) {
+                $missingEnvs[] = $key;
+            }
+        }
+
+        if (!empty($missingEnvs)) {
+            throw new RuntimeException(sprintf("Required ENV vars missing: ['%s']", implode("', '", $missingEnvs)));
+        }
+
+        return $this;
     }
 
-    if (!is_readable($this->_filepath)) {
-      throw new InvalidArgumentException(sprintf("Environment file '%s' is not readable.", $this->_filepath));
+    public function define() {
+        $this->requireParse('define');
+        foreach ($this->_environment as $key => $value) {
+            if (defined($key)) {
+                throw new LogicException(sprintf('Key "%s" has already been defined', $key));
+            }
+
+            define($key, $value);
+        }
+
+        return $this;
     }
 
-    $fc = file_get_contents($this->_filepath);
-    if ($fc === false) {
-      throw new InvalidArgumentException(sprintf("Environment file '%s' is not readable.", $this->_filepath));
+    public function toEnv($overwrite = false) {
+        $this->requireParse('toEnv');
+        foreach ($this->_environment as $key => $value) {
+            if (isset($_ENV[$key]) && !$overwrite) {
+                throw new LogicException(sprintf('Key "%s" has already been defined in $_ENV', $key));
+            }
+
+            $_ENV[$key] = $value;
+        }
+
+        return $this;
     }
 
-    $lines = explode(PHP_EOL, $fc);
+    public function toServer($overwrite = false) {
+        $this->requireParse('toServer');
+        foreach ($this->_environment as $key => $value) {
+            if (isset($_SERVER[$key]) && !$overwrite) {
+                throw new LogicException(sprintf('Key "%s" has already been defined in $_SERVER', $key));
+            }
 
-    $this->_environment = array();
-    foreach ($lines as $line) {
-      if (empty($line)) {
-        continue;
-      }
+            $_SERVER[$key] = $value;
+        }
 
-      if (!preg_match('/(?:export )?([a-zA-Z_][a-zA-Z0-9_]*)=(.*)/', $line, $matches)) {
-        continue;
-      }
-
-      $key = $matches[1];
-      $value = $matches[2];
-      if (preg_match('/^\'(.*)\'$/', $value, $matches)) {
-        $value = $matches[1];
-      } elseif (preg_match('/^"(.*)"$/', $value, $matches)) {
-        $value = $matches[1];
-      }
-
-      $this->_environment[$key] = $value;
+        return $this;
     }
 
-    return $this;
-  }
-
-  public function expect() {
-    $this->requireParse('expect');
-
-    $args = func_get_args();
-    if (count($args) == 0) {
-    	throw new InvalidArgumentException("No arguments were passed to expect()");
+    public function toArray() {
+        $this->requireParse('environment');
+        return $this->_environment;
     }
 
-		if (isset($args[0]) && is_array($args[0])) {
-			$args = $args[0];
-		}
-
-    $keys = (array) $args;
-    $missingEnvs = array();
-
-    foreach ($keys as $key) {
-      if (!isset($this->_environment[$key])) {
-        $missingEnvs[] = $key;
-      }
+    public function jsonSerialize() {
+        return $this->toArray();
     }
 
-    if (!empty($missingEnvs)) {
-      throw new RuntimeException(sprintf("Required ENV vars missing: ['%s']", implode("', '", $missingEnvs)));
+    public function __toString() {
+        return json_encode($this, JSON_PRETTY_PRINT);
     }
 
-    return $this;
-  }
-
-  public function define() {
-    $this->requireParse('define');
-    foreach ($this->_environment as $key => $value) {
-      if (defined($key)) {
-        throw new LogicException(sprintf('Key "%s" has already been defined', $key));
-      }
-
-      define($key, $value);
+    protected function requireParse($method) {
+        if (!is_array($this->_environment)) {
+            throw new LogicException(sprintf('Environment must be parsed before calling %s', $method));
+        }
     }
-
-    return $this;
-  }
-
-  public function toEnv($overwrite = false) {
-    $this->requireParse('toEnv');
-    foreach ($this->_environment as $key => $value) {
-      if (isset($_ENV[$key]) && !$overwrite) {
-        throw new LogicException(sprintf('Key "%s" has already been defined in $_ENV', $key));
-      }
-
-      $_ENV[$key] = $value;
-    }
-
-    return $this;
-  }
-
-  public function toServer($overwrite = false) {
-    $this->requireParse('toServer');
-    foreach ($this->_environment as $key => $value) {
-      if (isset($_SERVER[$key]) && !$overwrite) {
-        throw new LogicException(sprintf('Key "%s" has already been defined in $_SERVER', $key));
-      }
-
-      $_SERVER[$key] = $value;
-    }
-
-    return $this;
-  }
-
-  public function toArray() {
-    $this->requireParse('environment');
-    return $this->_environment;
-  }
-
-  public function jsonSerialize() {
-    return $this->toArray();
-  }
-
-  public function __toString() {
-    return json_encode($this, JSON_PRETTY_PRINT);
-  }
-
-  protected function requireParse($method) {
-    if (!is_array($this->_environment)) {
-      throw new LogicException(sprintf('Environment must be parsed before calling %s', $method));
-    }
-  }
-
 }
--- tests/josegonzalez/Dotenv/Load.php
+++ tests/josegonzalez/Dotenv/Load.php
@@ -1,172 +1,173 @@
 <?php
 
 use josegonzalez\Dotenv\Load;
+use \PHPUnit_Framework_TestCase;
 
-class LoadTest extends \PHPUnit_Framework_TestCase {
+class LoadTest extends PHPUnit_Framework_TestCase
+{
 
-  public function setUp() {
-    $this->env = $_ENV;
-    $this->server = $_SERVER;
-    $this->fixturePath = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR;
-    $this->Loader = new Loader($this->fixturePath . '.env');
-  }
+	public function setUp() {
+		$this->env = $_ENV;
+		$this->server = $_SERVER;
+		$this->fixturePath = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR;
+		$this->Loader = new Loader($this->fixturePath . '.env');
+	}
 
-  public function tearDown() {
-    $_ENV = $this->env;
-    $_SERVER = $this->server;
-    unset($this->env);
-    unset($this->server);
-    unset($this->fixturePath);
-    unset($this->Loader);
-  }
+	public function tearDown() {
+		$_ENV = $this->env;
+		$_SERVER = $this->server;
+		unset($this->env);
+		unset($this->server);
+		unset($this->fixturePath);
+		unset($this->Loader);
+	}
 
-  public function testFilepath() {
-    $this->assertEquals($this->fixturePath, $this->loader->filepath());
-  }
+	public function testFilepath() {
+		$this->assertEquals($this->fixturePath, $this->loader->filepath());
+	}
 
-  public function testSetFilepath() {
-    $this->Loader->setFilepath('/tmp/.env');
-    $this->assertEquals('/tmp/.env', $this->Loader->filepath());
-  }
+	public function testSetFilepath() {
+		$this->Loader->setFilepath('/tmp/.env');
+		$this->assertEquals('/tmp/.env', $this->Loader->filepath());
+	}
 
-  public function testParse() {
-    $this->Loader->parse();
-    $environment = $this->Loader->toArray();
+	public function testParse() {
+		$this->Loader->parse();
+		$environment = $this->Loader->toArray();
 
-    $this->assertEquals('bar', $environment['FOO']);
-    $this->assertEquals('baz', $environment['BAR']);
-    $this->assertEquals('with spaces', $environment['SPACED']);
-    $this->assertEquals('pgsql:host=localhost;dbname=test', $environment['EQUALS']);
-    $this->assertEquals('', $environment['NULL']);
-  }
+		$this->assertEquals('bar', $environment['FOO']);
+		$this->assertEquals('baz', $environment['BAR']);
+		$this->assertEquals('with spaces', $environment['SPACED']);
+		$this->assertEquals('pgsql:host=localhost;dbname=test', $environment['EQUALS']);
+		$this->assertEquals('', $environment['NULL']);
+	}
 
-  public function testParseExported() {
-    $this->Loader->setFilepath($this->fixturePath . 'exported.env');
-    $this->Loader->parse();
-    $environment = $this->Loader->toArray();
+	public function testParseExported() {
+		$this->Loader->setFilepath($this->fixturePath . 'exported.env');
+		$this->Loader->parse();
+		$environment = $this->Loader->toArray();
 
-    $this->assertEquals('bar', $environment['EFOO']);
-    $this->assertEquals('baz', $environment['EBAR']);
-    $this->assertEquals('with spaces', $environment['ESPACED']);
-    $this->assertEquals('pgsql:host=localhost;dbname=test', $environment['EEQUALS']);
-    $this->assertEquals('', $environment['ENULL']);
-  }
+		$this->assertEquals('bar', $environment['EFOO']);
+		$this->assertEquals('baz', $environment['EBAR']);
+		$this->assertEquals('with spaces', $environment['ESPACED']);
+		$this->assertEquals('pgsql:host=localhost;dbname=test', $environment['EEQUALS']);
+		$this->assertEquals('', $environment['ENULL']);
+	}
 
-  public function testParseExported() {
-    $this->Loader->setFilepath($this->fixturePath . 'quoted.env');
-    $this->Loader->parse();
-    $environment = $this->Loader->toArray();
+	public function testParseExported() {
+		$this->Loader->setFilepath($this->fixturePath . 'quoted.env');
+		$this->Loader->parse();
+		$environment = $this->Loader->toArray();
 
-    $this->assertEquals('bar', $environment['QFOO']);
-    $this->assertEquals('baz', $environment['QBAR']);
-    $this->assertEquals('with spaces', $environment['QSPACED']);
-    $this->assertEquals('pgsql:host=localhost;dbname=test', $environment['QEQUALS']);
-    $this->assertEquals('', $environment['QNULL']);
-  }
+		$this->assertEquals('bar', $environment['QFOO']);
+		$this->assertEquals('baz', $environment['QBAR']);
+		$this->assertEquals('with spaces', $environment['QSPACED']);
+		$this->assertEquals('pgsql:host=localhost;dbname=test', $environment['QEQUALS']);
+		$this->assertEquals('', $environment['QNULL']);
+	}
 
-  public function testExpect() {
-    $this->Loader->parse();
-    $this->assertTrue($this->Loader->expect('FOO'));
-    $this->assertTrue($this->Loader->expect(array('FOO', 'BAR')));
-  }
+	public function testExpect() {
+		$this->Loader->parse();
+		$this->assertTrue($this->Loader->expect('FOO'));
+		$this->assertTrue($this->Loader->expect(array('FOO', 'BAR')));
+	}
 
-  public function testDefine() {
-    $this->Loader->parse();
-    $this->Loader->define();
+	public function testDefine() {
+		$this->Loader->parse();
+		$this->Loader->define();
 
-    $this->assertEquals('bar', FOO);
-    $this->assertEquals('baz', BAR);
-    $this->assertEquals('with spaces', SPACED);
-    $this->assertEquals('pgsql:host=localhost;dbname=test', EQUALS);
-  }
+		$this->assertEquals('bar', FOO);
+		$this->assertEquals('baz', BAR);
+		$this->assertEquals('with spaces', SPACED);
+		$this->assertEquals('pgsql:host=localhost;dbname=test', EQUALS);
+	}
 
-  public function testToEnv() {
-    $this->Loader->parse();
-    $this->Loader->toEnv();
+	public function testToEnv() {
+		$this->Loader->parse();
+		$this->Loader->toEnv();
 
-    $this->assertEquals('bar', $_ENV['FOO']);
-    $this->assertEquals('baz', $_ENV['BAR']);
-    $this->assertEquals('with spaces', $_ENV['SPACED']);
-    $this->assertEquals('pgsql:host=localhost;dbname=test', $_ENV['EQUALS']);
-    $this->assertEquals('', $_ENV['NULL']);
-  }
+		$this->assertEquals('bar', $_ENV['FOO']);
+		$this->assertEquals('baz', $_ENV['BAR']);
+		$this->assertEquals('with spaces', $_ENV['SPACED']);
+		$this->assertEquals('pgsql:host=localhost;dbname=test', $_ENV['EQUALS']);
+		$this->assertEquals('', $_ENV['NULL']);
+	}
 
-  public function testToServer() {
-    $this->Loader->parse();
-    $this->Loader->toServer();
+	public function testToServer() {
+		$this->Loader->parse();
+		$this->Loader->toServer();
 
-    $this->assertEquals('bar', $_SERVER['FOO']);
-    $this->assertEquals('baz', $_SERVER['BAR']);
-    $this->assertEquals('with spaces', $_SERVER['SPACED']);
-    $this->assertEquals('pgsql:host=localhost;dbname=test', $_SERVER['EQUALS']);
-    $this->assertEquals('', $_SERVER['NULL']);
-  }
+		$this->assertEquals('bar', $_SERVER['FOO']);
+		$this->assertEquals('baz', $_SERVER['BAR']);
+		$this->assertEquals('with spaces', $_SERVER['SPACED']);
+		$this->assertEquals('pgsql:host=localhost;dbname=test', $_SERVER['EQUALS']);
+		$this->assertEquals('', $_SERVER['NULL']);
+	}
 
-  /**
-   * @expectedException LogicException
-   * @expectedExceptionMessage Environment must be parsed before calling expect()
-   */
-  public function testExpectRequireException() {
-    $this->Loader->expect();
-  }
+	/**
+	 * @expectedException LogicException
+	 * @expectedExceptionMessage Environment must be parsed before calling expect()
+	 */
+	public function testExpectRequireException() {
+		$this->Loader->expect();
+	}
 
-  /**
-   * @expectedException InvalidArgumentException
-   * @expectedExceptionMessage No arguments were passed to expect()
-   */
-  public function testExpectArgumentException() {
-    $this->Loader->parse();
-    $this->Loader->expect();
-  }
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage No arguments were passed to expect()
+	 */
+	public function testExpectArgumentException() {
+		$this->Loader->parse();
+		$this->Loader->expect();
+	}
 
-  /**
-   * @expectedException RuntimeException
-   * @expectedExceptionMessage Required ENV vars missing: ['BAZ']
-   */
-  public function testExpectMissingRequiredException() {
-    $this->Loader->parse();
-    $this->assertTrue($this->Loader->expect(array('BAZ')));
-  }
+	/**
+	 * @expectedException RuntimeException
+	 * @expectedExceptionMessage Required ENV vars missing: ['BAZ']
+	 */
+	public function testExpectMissingRequiredException() {
+		$this->Loader->parse();
+		$this->assertTrue($this->Loader->expect(array('BAZ')));
+	}
 
-  /**
-   * @expectedException LogicException
-   * @expectedExceptionMessage Environment must be parsed before calling define()
-   */
-  public function testDefineRequireException() {
-    $this->Loader->define();
-  }
+	/**
+	 * @expectedException LogicException
+	 * @expectedExceptionMessage Environment must be parsed before calling define()
+	 */
+	public function testDefineRequireException() {
+		$this->Loader->define();
+	}
 
-  /**
-   * @expectedException LogicException
-   * @expectedExceptionMessage Key "NULL" has already been defined
-   */
-  public function testDefineNullException() {
-    $this->Loader->parse();
-    $this->Loader->define();
-  }
+	/**
+	 * @expectedException LogicException
+	 * @expectedExceptionMessage Key "NULL" has already been defined
+	 */
+	public function testDefineNullException() {
+		$this->Loader->parse();
+		$this->Loader->define();
+	}
 
-  /**
-   * @expectedException LogicException
-   * @expectedExceptionMessage Environment must be parsed before calling toEnv()
-   */
-  public function testToEnvRequireException() {
-    $this->Loader->toEnv();
-  }
+	/**
+	 * @expectedException LogicException
+	 * @expectedExceptionMessage Environment must be parsed before calling toEnv()
+	 */
+	public function testToEnvRequireException() {
+		$this->Loader->toEnv();
+	}
 
-  /**
-   * @expectedException LogicException
-   * @expectedExceptionMessage Environment must be parsed before calling toServer()
-   */
-  public function testToServerRequireException() {
-    $this->Loader->toServer();
-  }
+	/**
+	 * @expectedException LogicException
+	 * @expectedExceptionMessage Environment must be parsed before calling toServer()
+	 */
+	public function testToServerRequireException() {
+		$this->Loader->toServer();
+	}
 
-  /**
-   * @expectedException LogicException
-   * @expectedExceptionMessage Environment must be parsed before calling toArray()
-   */
-  public function testToArrayException() {
-    $this->Loader->toArray();
-  }
-
+	/**
+	 * @expectedException LogicException
+	 * @expectedExceptionMessage Environment must be parsed before calling toArray()
+	 */
+	public function testToArrayException() {
+		$this->Loader->toArray();
+	}
 }
