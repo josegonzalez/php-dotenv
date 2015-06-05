d647e50fa4aecc4970a365d4f0621196565a5508
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-06-05 01:54:50 -0400

    Fix issue where first fail would result in not reading the file

--- src/josegonzalez/Dotenv/Loader.php
+++ src/josegonzalez/Dotenv/Loader.php
@@ -142,22 +142,23 @@ class Loader
         $contents = false;
         $filepaths = $this->filepaths();
 
-        foreach ($filepaths as $filepath) {
-            if (!file_exists($filepath)) {
+        foreach ($filepaths as $i => $filepath) {
+            $isLast = count($filepaths) - 1 === $i;
+            if (!file_exists($filepath) && $isLast) {
                 return $this->raise(
                     'InvalidArgumentException',
                     sprintf("Environment file '%s' is not found", $filepath)
                 );
             }
 
-            if (is_dir($filepath)) {
+            if (is_dir($filepath) && $isLast) {
                 return $this->raise(
                     'InvalidArgumentException',
                     sprintf("Environment file '%s' is a directory. Should be a file", $filepath)
                 );
             }
 
-            if (!is_readable($filepath) || ($contents = file_get_contents($filepath)) === false) {
+            if ((!is_readable($filepath) || ($contents = file_get_contents($filepath)) === false) && $isLast) {
                 return $this->raise(
                     'InvalidArgumentException',
                     sprintf("Environment file '%s' is not readable", $filepath)
